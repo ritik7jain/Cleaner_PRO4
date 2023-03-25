d447f2f1398f5f746d5cc65104f9204b1ba2a5b5
RITIK JAIN <jritik324@gmail.com>
2023-03-25 20:30:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,17 @@
 import streamlit as st
-from datetime import date, datetime, timedelta
 import pandas as pd
-import csv
+from datetime import date, timedelta, datetime
 
-data = {
-    'Room': ['101', '102', '103', '104', '105'],
-    'Status': ['Clean', 'Clean', 'Clean', 'Clean', 'Clean'],
-    'Last Cleaned': ['2023-03-24', '2023-03-23', '2023-03-22', '2023-03-21', '2023-03-20']
-}
-df = pd.DataFrame(data)
-# status_options = ['All', 'Clean', 'Dirty']
-
-
-# selected_status = st.sidebar.selectbox('Select status', status_options)
-
+df = pd.read_csv('data.csv')
 sort_by_last_cleaned = st.sidebar.checkbox('Sort by last cleaned date')
 
-
-# if selected_status == 'All':
-#     filtered_df = df
-# else:
-#     filtered_df = df[df['Status'] == selected_status]
-# if sort_by_last_cleaned:
-#     df = df.sort_values('Last Cleaned')
-        
 for index, row in df.iterrows():
-    if df.at[index,'Last Cleaned'] != str(date.today()-timedelta(days=1)):
-        df.at[index,'Status']='Dirty'
+    last_cleaned_date = datetime.strptime(df.at[index,'Last Cleaned'], '%Y-%m-%d').date()
+    days_since_cleaned = (date.today() - last_cleaned_date).days
+    
+    if days_since_cleaned >= 2:  # room was last cleaned on 24 and today is 26 or later
+        df.at[index,'Status'] = 'Dirty'
+
         
 for index, row in df.iterrows():
     new_status = st.selectbox(f"Room {row['Room']}: {row['Status']}", ['', 'Clean'])
@@ -34,8 +19,7 @@ for index, row in df.iterrows():
     if new_status:
         if new_status!= df.at[index,'Status']:
             df.at[index, 'Last Cleaned'] = str(date.today())
-            if sort_by_last_cleaned:
-                df = df.sort_values('Last Cleaned')
         df.at[index, 'Status'] = new_status
 
+df.to_csv('data.csv', index=False)
 st.table(df)
